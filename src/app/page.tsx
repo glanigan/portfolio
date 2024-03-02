db7548e8aaf373b53b2bca85ac84a0f61ddad73d
Gary Lanigan <garylanigan@live.co.uk>
2024-03-02 19:18:08 +0000

    initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="max-w-screen-md mx-auto flex min-h-screen flex-col items-center p-24 justify-center">
+      <p className="text-2xl"><span className='text-purple-400'>Hello</span>, I'm Gary a typescript developer who loves building amazing experiences on web.</p>
+    </main>
+  );
+}
